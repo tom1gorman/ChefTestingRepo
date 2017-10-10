1abef611e9a74038afabe65d6fd33b868e1f1dc5
“Tom <tom.gorman@phoenix.edu>
2017-10-10 19:13:17 +0000

    Initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,22 @@
+package 'ntp' do
+  action :install
+end
+
+service 'ntpd' do
+  action [ :enable, :start ]
+end
+
+package 'tree' do
+  action :install
+end
+
+file '/etc/motd' do
+  content 'This server is the property of Tom Gorman'
+  action :create
+  owner 'root'
+  group 'root'
+end
+
+package 'git' do
+  action :install
+end
